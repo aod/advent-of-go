d4b1c948137c14af9104768b90b27eef3db076fb
Oktay <24858380+aoktayd@users.noreply.github.com>
2018-10-12 11:44:36 +0200

    refactors reading of input file for puzzles

--- 2017/01/main.go
+++ 2017/01/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	input := ParseInput(utils.PuzzleInput(2017, 1))
+	input := ParseInput(utils.RelFile("input.txt"))
 
 	fmt.Printf("Part 1: %d\n", Solution(input, 1))
 	fmt.Printf("Part 2: %d\n", Solution(input, 2))
--- utils/input.go
+++ utils/input.go
@@ -1,23 +1,15 @@
 package utils
 
 import (
-	"fmt"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"runtime"
 )
 
-// PuzzleInput Get the input of the year/day
-func PuzzleInput(year uint16, day byte) []byte {
-	_, file, _, _ := runtime.Caller(0)
-
-	challengePath := fmt.Sprintf("%d/%02d/input.txt", year, day)
-	path := filepath.Join(file, fmt.Sprintf("../../%s", challengePath))
-
-	if len(os.Args) > 1 {
-		return []byte(os.Args[1])
-	}
+// RelFile Gets a file relative to the function caller
+func RelFile(fileName string) []byte {
+	_, file, _, _ := runtime.Caller(1)
+	path := filepath.Join(filepath.Dir(file), fileName)
 
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
