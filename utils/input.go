2e74ef9d76179313629118abfe20c4c13660fcb4
Oktay <24858380+aoktayd@users.noreply.github.com>
2018-10-12 19:27:54 +0200

    more refactoring

--- utils/input.go
+++ utils/input.go
@@ -6,15 +6,12 @@ import (
 	"runtime"
 )
 
-// RelFile Gets a file relative to the function caller
-func RelFile(fileName string) []byte {
+// ReadRelFile Reads a file relative to the function caller
+func ReadRelFile(fileName string) ([]byte, error) {
 	_, file, _, _ := runtime.Caller(1)
 	path := filepath.Join(filepath.Dir(file), fileName)
 
 	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
 
-	return data
+	return data, err
 }
