d8f2b018dc01d4b553b84955d8cd765e1740a5b1
Oktay Dinler <24858380+aoktayd@users.noreply.github.com>
2019-03-10 21:24:39 +0100

    completes 2018/05 part 1 & 2

---
+++ 2018/05/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aoktayd/adventofgode/internal/input"
+)
+
+const unitDifference byte = 32
+
+func react(polymer []byte) ([]byte, bool) {
+	hasReacted := false
+	reactionAt := 0
+
+	for i := 0; i < len(polymer)-1; i++ {
+		firstUnit := polymer[i]
+		neighbouringUnit := polymer[i+1]
+
+		if firstUnit+unitDifference == neighbouringUnit || neighbouringUnit+unitDifference == firstUnit {
+			hasReacted = true
+			reactionAt = i
+			break
+		}
+	}
+
+	polymer = append(polymer[:reactionAt], polymer[reactionAt+2:]...)
+
+	return polymer, hasReacted
+}
+
+func chainReaction(polymer []byte) []byte {
+	polymerCopy := append([]byte{}, polymer...)
+	for {
+		newPolymer, hasReacted := react(polymerCopy)
+		if !hasReacted {
+			break
+		}
+		polymerCopy = newPolymer
+	}
+	return polymerCopy
+}
+
+func main() {
+	input := input.Read(input.File{Puzzle: input.Puzzle{Year: 2018, Day: 5}, FileType: ".txt"})
+
+	part1 := chainReaction(input)
+
+	const baseUnit = 65
+	const unitTypes = 25
+	smallestPolymer := 50000 // input size
+
+	for i := 0; i <= unitTypes; i++ {
+		unitToRemove := byte(baseUnit + i)
+
+		var polymer []byte
+		for _, unit := range input {
+			if unit == unitToRemove || unit == unitToRemove+unitDifference {
+				continue
+			}
+			polymer = append(polymer, unit)
+		}
+
+		if len(input) == len(polymer) {
+			continue
+		}
+
+		polymer = chainReaction(polymer)
+		polymerLength := len(polymer)
+
+		if polymerLength < smallestPolymer {
+			smallestPolymer = polymerLength
+		}
+	}
+
+	fmt.Println("Part 1: ", len(part1))
+	fmt.Println("Part 2: ", smallestPolymer)
+}
